9dca04ac721b505495cc5be8dfdec3d136d640ab
Jackson Barr <jbarr39@gatech.edu>
2024-11-24 17:55:55 -0500

    Add log out button

--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -1,13 +1,17 @@
 import '../styles.css';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 function HomePage() {
     const navigate = useNavigate();
+    const { logout } = useContext(UserContext);
 
     return (
         <div>
             <p>Home</p>
             <button onClick={() => {navigate('/profile')}}>Profile</button>
+            <button onClick={logout}>Log Out</button>
         </div>
     );
 }
