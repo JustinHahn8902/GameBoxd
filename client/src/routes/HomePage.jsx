4bc43d092549ce41e6b21c8cf14ef3581f97b403
Justin Hahn <jdhahn.2003@gmail.com>
2024-11-24 16:14:12 -0500

    Profile page changes persist in user context

--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -1,9 +1,13 @@
 import '../styles.css';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+    const navigate = useNavigate();
+
     return (
         <div>
             <p>Home</p>
+            <button onClick={() => {navigate('/profile')}}>Profile</button>
         </div>
     );
 }
