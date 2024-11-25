d15b13cf3ee027bff15130ea59fec01fd6c3dabf
Jackson Barr <jbarr39@gatech.edu>
2024-11-25 14:59:13 -0500

    Create basic list endpoints

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const gameRoutes = require('./routes/games');
+const listRoutes = require('./routes/lists');
 
 const createApp = () => {
   const app = express();
@@ -18,6 +19,7 @@ const createApp = () => {
   // Routes
   app.use('/api/auth', authRoutes);
   app.use('/api/games', gameRoutes);
+  app.use('/api/lists', listRoutes);
 
 
   return app;
