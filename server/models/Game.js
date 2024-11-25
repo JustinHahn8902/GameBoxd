5753110ffaa175a8ece3a41f42850874c9f0cf61
Jackson Barr <jbarr39@gatech.edu>
2024-11-24 20:41:06 -0500

    Create logic to fully fetch game data

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema({
+    igdb_id: Number,
     name: String,
     genres: [String],
-    release_date: Date
+    release_date: Date,
+    cover_url: String,
+    screenshot_urls: [String],
+    similar_games: [Number],
+    total_rating: Number,
+    total_rating_count: Number,
+    summary: String,
+    website_urls: [String],
+    platforms: [String]
 });
 
 const Game = mongoose.model('Game', gameSchema);
